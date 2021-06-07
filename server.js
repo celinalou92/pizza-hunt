48e57af004a2466bb4487e4763374bab0a685776
Celina Louissaint <celinalouissaint@gmail.com>
2021-06-06 20:25:29 -0700

    CRUD methods for pizza completed

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+// add mongoose at start
+const mongoose = require('mongoose');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,5 +11,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.use(require('./routes'));
+
+
+
+// mongoose.connect tells mongoose which database to connect to 
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Use this to log mongo queries being executed!
+mongoose.set('debug', true);
+
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
